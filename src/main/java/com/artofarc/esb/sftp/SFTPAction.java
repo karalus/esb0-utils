ddd71cf13c71c96c5437130c519d243d82442e05
Andre Karalus <andre@karalus-it.de>
2025-07-09 13:42:29 +0200

    SFTPAction: Default content type is application/octet-stream

--- src/main/java/com/artofarc/esb/sftp/SFTPAction.java
+++ src/main/java/com/artofarc/esb/sftp/SFTPAction.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2025 Andre Karalus
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.artofarc.esb.sftp;
 
 import java.io.FileNotFoundException;
@@ -92,7 +107,8 @@ public class SFTPAction extends Action {
 				switch (verb) {
 				case "GET":
 					message.reset(BodyType.INPUT_STREAM, session.channelSftp.get(filename));
-					message.setContentType(MimeHelper.guessContentTypeFromName(filename));
+					String contentType = MimeHelper.guessContentTypeFromName(filename);
+					message.setContentType(contentType != null ? contentType : HttpConstants.HTTP_HEADER_CONTENT_TYPE_OCTET_STREAM);
 					message.putHeader(HttpConstants.HTTP_HEADER_CONTENT_DISPOSITION, "filename=\"" + filename + '"');
 					message.putVariable(ESBConstants.HttpResponseCode, HttpConstants.SC_OK);
 					break;
